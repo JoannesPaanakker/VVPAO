cf090b1fd33c2ca1a95a02dc4eeae599baffc9e5
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-12-08 15:49:58 +0100

    fix expertise list for admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,7 @@ class AdminController < ApplicationController
   def dashboard
     @newusers = User.where(member: false)
     @members = User.where(member: true).sort_by{ |user| user.last_name }
-    @expertises = Expertise.all.sort_by{ |exp| exp.name.downcase }
+    @expertises = Expertise.all.drop(1).sort_by{ |exp| exp.name.downcase }
     @expertise = Expertise.new
     @targetaudiences = Targetaudience.all.sort_by{ |aud| aud.name.downcase }
     @targetaudience = Targetaudience.new
