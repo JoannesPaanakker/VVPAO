96ff1e22206acb2855a085e5ec761fefec0d8bcb
Joannes Paanakker <Paanakker@qedwebdevelopment.com>
2019-01-14 15:37:18 +0100

    bestanden pagina

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :smtp
   # Do care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
