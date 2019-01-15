0642c32ade3eab4b8dd100bf59f692e963e8f4a9
Joannes Paanakker <Paanakker@qedwebdevelopment.com>
2019-01-15 13:44:11 +0100

    fix bugs 2 and 3

--- app/javascript/packs/psychiaterslist.js
+++ app/javascript/packs/psychiaterslist.js
@@ -3,7 +3,7 @@ import { expertiseFilter } from "./partials/expertisefilter"
 import { startMap } from "./partials/googlemaps.js.erb"
 import { getLatLng, getLatLngBatch } from "./partials/geocode"
 
-const dev001 = document.getElementById("kaboom").addEventListener("click", getLatLngBatch);
+// const dev001 = document.getElementById("kaboom").addEventListener("click", getLatLngBatch);
 startMap();
 
 console.log("hello from psychiaterslist.js")
